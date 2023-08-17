1fefbaa85559f2e284bcb9f91a65df73bef52f19
FELIPE DOS REIS TAVARES <felipe.tavares@granbery.edu.dti>
2023-08-16 21:55:56 -0300

    att

---
+++ dicionario.py
@@ -0,0 +1,9 @@
+
+pessoa = {
+    "nome": "João",
+    "idade": 30,
+    "cidade": "São Paulo"
+}
+print(pessoa)
+
+
---
+++ listavitor.py
@@ -0,0 +1,4 @@
+numeros_pares = []
+for i in range(0,1000, 2):
+    numeros_pares.append(i)
+print(numeros_pares)
